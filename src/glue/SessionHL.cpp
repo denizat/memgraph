75616f66681b98ec197ab3b31b3363fc8ac0ce66
Andreja Tonev <andreja.tonev@memgraph.io>
2024-03-29 18:15:04 +0100

    Allow user to connect without database

--- src/glue/SessionHL.cpp
+++ src/glue/SessionHL.cpp
@@ -161,9 +161,18 @@ bool SessionHL::Authenticate(const std::string &username, const std::string &pas
   }
 #ifdef MG_ENTERPRISE
   // Start off with the default database
-  interpreter_.SetCurrentDB(GetDefaultDB(), false);
+  try {
+    interpreter_.SetCurrentDB(GetDefaultDB(), false);
+  } catch (auth::AuthException &) {
+    // Failed to get default db, connect without db
+    interpreter_.ResetDB();
+  }
 #endif
-  implicit_db_.emplace(GetCurrentDB());
+  const auto db = GetCurrentDB();
+  if (db.empty())
+    implicit_db_.reset();
+  else
+    implicit_db_.emplace(std::move(db));
   return res;
 }
 
@@ -254,7 +263,8 @@ using memgraph::communication::bolt::Value;
 #ifdef MG_ENTERPRISE
 auto SessionHL::Route(std::map<std::string, Value> const &routing,
                       std::vector<memgraph::communication::bolt::Value> const & /*bookmarks*/,
-                      std::map<std::string, Value> const & /*extra*/) -> std::map<std::string, Value> {
+                      std::map<std::string, Value> const &
+                      /*extra*/) -> std::map<std::string, Value> {
   auto routing_map = ranges::views::transform(
                          routing, [](auto const &pair) { return std::pair(pair.first, pair.second.ValueString()); }) |
                      ranges::to<std::map<std::string, std::string>>();
@@ -347,7 +357,7 @@ void SessionHL::Configure(const std::map<std::string, memgraph::communication::b
     if (!in_explicit_db_) implicit_db_.emplace(current);  // Still not in an explicit database, save for recovery
     in_explicit_db_ = true;
     // NOTE: Once in a transaction, the drivers stop explicitly sending the db and count on using it until commit
-  } else if (in_explicit_db_ && !interpreter_.in_explicit_transaction_) {  // Just on a switch
+  } else if (!implicit_db_ || (in_explicit_db_ && !interpreter_.in_explicit_transaction_)) {  // Just on a switch
     if (implicit_db_) {
       db = *implicit_db_;
     } else {
@@ -382,7 +392,7 @@ SessionHL::SessionHL(memgraph::query::InterpreterContext *interpreter_context,
 #endif
       auth_(auth),
       endpoint_(std::move(endpoint)),
-      implicit_db_(dbms::kDefaultDB) {
+      implicit_db_(std::nullopt) {
   // Metrics update
   memgraph::metrics::IncrementCounter(memgraph::metrics::ActiveBoltSessions);
 #ifdef MG_ENTERPRISE
